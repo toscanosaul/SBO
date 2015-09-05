9d528b50c5b7eb046150e02428062192139395f4
Saul Toscano Palmerin <st684@login-1.icse.cornell.edu>
2015-09-05 15:43:37 -0400

    plots

--- Python/AnalysisResultsPlots.py
+++ Python/AnalysisResultsPlots.py
@@ -16,6 +16,7 @@ numberIterations=19
 numberPrior=5
 directory=os.path.join("AnalyticExample","Results"+"%d"%samplesIteration+"AveragingSamples"+"%d"%numberPrior+"TrainingPoints")
 directory2=os.path.join("ExpectedImprovement","AnalyticExample","Results"+"%d"%samplesIteration+"AveragingSamples"+"%d"%numberPrior+"TrainingPoints")
+directory3=os.path.join("KnowledgeGradient","AnalyticExample","Results"+"%d"%samplesIteration+"AveragingSamples"+"%d"%numberPrior+"TrainingPoints")
 
 
 x=np.linspace(0,samplesIteration*numberIterations,numberIterations+1)
@@ -45,7 +46,7 @@ y2=np.zeros([972,numberIterations+1])
 cont=0
 for i in range(1,repetitions+1):
     temp=np.loadtxt(os.path.join(directory2,"EI","%d"%i+"run","%d"%i+"optimalValues.txt"))
-    if len(temp)>=(numberIterations+1)*2:
+    if len(temp)>=(numberIterations+1)*2 and cont<972:
         for j in range(numberIterations+1):
             y2[cont,j]=temp[2*j]
         cont+=1
@@ -64,13 +65,41 @@ confidence=means-1.96*(var**.5)/np.sqrt(cont)
 plt.plot(x,confidence,'--',color='g')
 
 
+#y2=np.zeros([972,numberIterations+1])
+y2=np.zeros([0,numberIterations+1])
+cont=0
+for i in range(1,repetitions+1):
+    try:
+    	temp=np.loadtxt(os.path.join(directory3,"KG","%d"%i+"run","%d"%i+"optimalValues.txt"))
+        if len(temp)>=(numberIterations+1)*2:
+	    temp1=np.zeros((numberIterations+1))
+            for j in range(numberIterations+1):
+                temp1[j]=temp[2*j]
+	    y2=np.vstack((y2,temp1))
+            cont+=1
+    except:
+           continue
+  #  y[i-1,:]=np.loadtxt("%d"%i+"G(xn)KGAnalytic.txt")
+print cont
+
+for i in xrange(numberIterations+1):
+    means[i]=np.mean(y2[:,i])
+    var[i]=np.var(y2[:,i])
+
+
+plt.plot(x,means,color='b',linewidth=2.0,label='KG')
+confidence=means+1.96*(var**.5)/np.sqrt(cont)
+plt.plot(x,confidence,'--',color='g',label="95% CI")
+confidence=means-1.96*(var**.5)/np.sqrt(cont)
+plt.plot(x,confidence,'--',color='g')
+
 
 
 plt.xlabel('Number of Samples',fontsize=26)
 plt.ylabel('Optimum Value of G',fontsize=24)
 plt.legend(loc=3,
            ncol=2, mode="expand", borderaxespad=0.)
-plt.savefig(os.path.join(directory,"comparisonDifferentMethodsIncludesEI.pdf"))
+plt.savefig(os.path.join(directory,"comparisonDifferentMethodsIncludesEIKG.pdf"))
 plt.close("all")
 
 
