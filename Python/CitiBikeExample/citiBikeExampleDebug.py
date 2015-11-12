51ae92f943ead8d2cc61baa29ec61253e88f6824
Saul Toscano <toscano.saul@gmail.com>
2015-11-11 21:42:35 -0500

    ,

--- Python/CitiBikeExample/citiBikeExampleDebug.py
+++ Python/CitiBikeExample/citiBikeExampleDebug.py
@@ -501,6 +501,7 @@ w1=wSt[0:0+1,:]
 tempN=sboObj.numberTraining
 st=np.concatenate((x1,w1),1)
 
+
 print "st"
 print st
 
@@ -529,8 +530,8 @@ sboObj.functionGradientAscentVn(st,True,sboObj,0,L,temp2,a,sboObj.Bhist,scratch)
 print "second iteration"
 
 #####second iteration
-xTrans=sboObj.transformationDomainX(x1[0:1,0:sboObj.dimXsteepest])
-wTrans=sboObj.transformationDomainW(x1[0:1,sboObj.dimXsteepest:sboObj.dimXsteepest+sboObj._dimW])
+xTrans=sboObj.transformationDomainX(st[0:1,0:sboObj.dimXsteepest])
+wTrans=sboObj.transformationDomainW(st[0:1,sboObj.dimXsteepest:sboObj.dimXsteepest+sboObj._dimW])
 ###falta transformar W
 temp=np.concatenate((xTrans,wTrans),1)
 print temp
