843c6117782700c03e7dbb4b342a94abb5301a74
Saul Toscano <toscano.saul@gmail.com>
2015-11-16 21:28:21 -0500

    ,

--- Python/CitiBikeExample/KG.py
+++ Python/CitiBikeExample/KG.py
@@ -296,9 +296,9 @@ class KG:
         self.optPointsArray=[]
     
     def trainModel(self,numStarts,**kwargs):
-        self._k.train(scaledAlpha=self.scaledAlpha,numStarts=numStarts,**kwargs)
+        self.stat._k.train(scaledAlpha=self.scaledAlpha,numStarts=numStarts,**kwargs)
         
         f=open(os.path.join(self.path,'%d'%self.randomSeed+"hyperparameters.txt"),'w')
-        f.write(str(self._k.getParamaters()))
+        f.write(str(self.stat._k.getParamaters()))
         f.close()
         
