04a4b66773856f2a69bb30e38eba0058e0424c97
Saul Toscano <toscano.saul@gmail.com>
2015-11-24 18:57:45 -0500

    ,

--- CitiBike/poissonGeneratePointsRandom.py
+++ CitiBike/poissonGeneratePointsRandom.py
@@ -5,16 +5,6 @@ import numpy as np
 
 
 if __name__ == "__main__":
-    result=[]
-    np.random.seed(1)
-    nBikes=6000
-    nPoints=1000
-    s=nBikes*np.random.dirichlet(np.ones(4),nPoints)
-    tpm=np.floor(s[:,0:3])
-    col4=nBikes-np.sum(tpm,1)
-    col4=col4.reshape((len(col4),1))
-    s=np.concatenate((tpm,col4),1)
-    temp=s
     n1=4
     f = open(str(4)+"-cluster.txt", 'r')
     cluster=eval(f.read())
@@ -23,36 +13,27 @@ if __name__ == "__main__":
     bikeData=np.loadtxt("bikesStationsOrdinalIDnumberDocks.txt",skiprows=1)
     upperX=np.zeros(n1)
     temBikes=bikeData[:,2]
+
     for i in xrange(n1):
         temp=cluster[i]
         indsTemp=np.array([a[0] for a in temp])
         upperX[i]=np.sum(temBikes[indsTemp])
-    ####
+        
+    result=[]
+    np.random.seed(1)
+    nBikes=6000
+    nPoints=1000
+    s=np.random.dirichlet(np.ones(4),nPoints)
+    s[:,0]*=upperX[0]
+    s[:,0]=np.floor(s[:,0])
+    s[:,1]*=upperX[1]
+    s[:,1]=np.floor(s[:,1])
     for j in range(nPoints):
-        ind=np.where(temp[:,j]-upperX<0)[0]
-        temp[ind,j]=upperX[ind]
-        if len(ind)>0:
-            res=np.sum(temp[ind,j]-upperX[ind])
-            while (res>0):
-                ind2=np.where(temp[:,j]-upperX>0)[0]
-                
-                partialBikes=res/len(ind2)
-                resBikes=res%len(ind2)
-                putBikes=0
-                for k in ind2:
-                    tmp=min(upperX[k],partialBikes+temp[k,j])
-                    putBikes+=tmp-temp[k,j]
-                    temp[k,j]=tmp
-                if putBikes==0:
-                    for k in ind2:
-                        tmp=min(upperX[k],res+temp[k,j])
-                        putBikes+=tmp-temp[k,j]
-                        temp[k,j]=tmp
-                        if putBikes==res:
-                            break
-                    break
-                pendingBikes=partialBikes*len(ind2)-putBikes
-                res=resBikes+pendingBikes
-    f=open("NewpointsPoisson1000.txt","w")
-    np.savetxt(f,temp)
+        s[j,2]=s[j,2]*min(nBikes-s[j,0]-s[j,1],upperX[2])+(1-s[j,2])*max(nBikes-s[j,0]-s[j,1]-upperX[3],0)
+        s[j,2]=np.floor(s[j,2])
+        s[j,3]=nBikes-np.sum(s[j,0:3])
+    
+    
+    f=open("NewRandompointsPoisson1000.txt","w")
+    np.savetxt(f,s)
     f.close()
