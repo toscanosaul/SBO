70dbe65ee994a77a0d5ae52113fb1a7782910e5d
Saul Toscano <toscano.saul@gmail.com>
2015-11-24 19:54:35 -0500

    ,

--- CitiBike/poissonGeneratePointsRandom.py
+++ CitiBike/poissonGeneratePointsRandom.py
@@ -24,16 +24,16 @@ if __name__ == "__main__":
     nBikes=6000
     nPoints=10000
     s=np.random.dirichlet(np.ones(4),nPoints)
-    s[:,0]*=upperX[0]
+    s[:,0]=s[:,0]*upperX[0]+(1-s[:,0])*500
     s[:,0]=np.floor(s[:,0])
-    s[:,1]*=upperX[1]
+    s[:,1]=s[:,0]*upperX[1]+(1-s[:,0])*500
     s[:,1]=np.floor(s[:,1])
     for j in range(nPoints):
-        s[j,2]=s[j,2]*min(nBikes-s[j,0]-s[j,1],upperX[2])+(1-s[j,2])*max(nBikes-s[j,0]-s[j,1]-upperX[3],0)
+        s[j,2]=s[j,2]*min(nBikes-s[j,0]-s[j,1],upperX[2])+(1-s[j,2])*max(nBikes-s[j,0]-s[j,1]-upperX[3],500)
         s[j,2]=np.floor(s[j,2])
         s[j,3]=nBikes-np.sum(s[j,0:3])
+        
     
-    
-    f=open("NewRandompointsPoisson1000.txt","w")
+    f=open("lowerBoundNewRandompointsPoisson1000.txt","w")
     np.savetxt(f,s)
     f.close()
