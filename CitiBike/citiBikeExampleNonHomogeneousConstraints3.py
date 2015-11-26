05b744ea27552773bd25c2e69d9f05629cf2c4e9
Saul Toscano <toscano.saul@gmail.com>
2015-11-26 17:33:33 -0500

    ,

--- CitiBike/citiBikeExampleNonHomogeneousConstraints3.py
+++ CitiBike/citiBikeExampleNonHomogeneousConstraints3.py
@@ -275,7 +275,7 @@ trainingPoints=nTemp2
 #nameDirectory="Results"+'%d'%numberSamplesForF+"AveragingSamples"+'%d'%trainingPoints+"TrainingPoints"
 #folder=os.path.join(nameDirectory,"SBO")
 
-misc=inter.Miscellaneous(randomSeed,parallel,nF=numberSamplesForF,tP=trainingPoints,prefix="TestFinalNonHomogeneous2")
+misc=inter.Miscellaneous(randomSeed,parallel,nF=numberSamplesForF,tP=trainingPoints,prefix="TestFinalNonHomogeneous3")
 
 """
 We define the data object.
