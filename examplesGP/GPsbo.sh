c1849e6759ddcd5db38e53898b7774079f3d82ba
Saul Toscano <toscano.saul@gmail.com>
2015-12-28 14:15:42 -0600

    ,

--- examplesGP/GPsbo.sh
+++ examplesGP/GPsbo.sh
@@ -29,3 +29,16 @@ for i in {0..10}; do
          done
     done
 done
+
+
+for i in {0..10}; do
+    let "var=2**$i"
+    var=$( echo "1.0/$var" | bc -l)    
+    for n in {1,2,4,8,16}; do
+        for j in {1..10}; do
+            A=$(echo "1.0/($j * $n)" | bc -l)
+            for k in $(seq $BEGIN $END);
+                do jsub "progGPkg.nbs $k $n 100 $var $A F" -mfail -email toscano.saul@gmail.com -xhost sys_pf -queue long; done
+         done
+    done
+done
