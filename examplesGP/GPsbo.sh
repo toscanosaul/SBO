6d1171ac1879b50a14335f26078827386a29f765
Saul Toscano Palmerin <toscano.saul@gmail.com>
2015-12-28 20:46:30 -0500

    ,

--- examplesGP/GPsbo.sh
+++ examplesGP/GPsbo.sh
@@ -3,26 +3,13 @@ BEGIN=$2
 
 
 
-for i in {0..10}; do
-    let "var=2**$i"
-    var=$( echo "1.0/$var" | bc -l)    
-    echo $var
-    for n in {1,2,4,8,16}; do
-        echo $n
-        for j in {1..10}; do
-            A=$(echo "1.0/($j * $n)" | bc -l)
-            echo $A
-            jsub "progGP.nbs 1 $n 1 $var $A T" -mfail -email toscano.saul@gmail.com -xhost sys_pf -queue long; 
-         done
-    done
-done
 
 
-for i in {0..10}; do
+for i in {0..4}; do
     let "var=2**$i"
     var=$( echo "1.0/$var" | bc -l)    
     for n in {1,2,4,8,16}; do
-        for j in {1..10}; do
+        for j in {2,4,8,16}; do
             A=$(echo "1.0/($j * $n)" | bc -l)
             for k in $(seq $BEGIN $END);
                 do jsub "progGP.nbs $k $n 100 $var $A F" -mfail -email toscano.saul@gmail.com -xhost sys_pf -queue long; done
@@ -31,14 +18,14 @@ for i in {0..10}; do
 done
 
 
-for i in {0..10}; do
-    let "var=2**$i"
-    var=$( echo "1.0/$var" | bc -l)    
-    for n in {1,2,4,8,16}; do
-        for j in {1..10}; do
-            A=$(echo "1.0/($j * $n)" | bc -l)
-            for k in $(seq $BEGIN $END);
-                do jsub "progGPkg.nbs $k $n 100 $var $A F" -mfail -email toscano.saul@gmail.com -xhost sys_pf -queue long; done
-         done
-    done
-done
+#for i in {0..4}; do
+#    let "var=2**$i"
+#    var=$( echo "1.0/$var" | bc -l)    
+#    for n in {1,2,4,8,16}; do
+#        for j in {2,4,8,16}; do
+#            A=$(echo "1.0/($j * $n)" | bc -l)
+#            for k in $(seq $BEGIN $END);
+#                do jsub "progGPkg.nbs $k $n 100 $var $A F" -mfail -email toscano.saul@gmail.com -xhost sys_pf -queue long; done
+#         done
+#    done
+#done
