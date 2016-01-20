ae6b083e91c1ca83fa47e29f0dca10bf497588cd
Saul Toscano <toscano.saul@gmail.com>
2016-01-20 00:27:37 -0600

    ,

--- examplesGP/plosPaper.py
+++ examplesGP/plosPaper.py
@@ -25,7 +25,7 @@ numberPrior=30
 A=[2,4,8,16]
 #A=[2]
 varianceb=[1.0/(2.0**k) for k in xrange(5)]#+[2.0**r for r in range(1,5)]
-varianceb=[(k-4.0) for k in xrange(5)]+
+varianceb=[(k-4.0) for k in xrange(5)]+[k for k in range(1,9)]
 #varianceb=[1.0/(2.0**k) for k in xrange(2)]
 
 numberdifIteration=len(samplesIteration)
@@ -114,21 +114,27 @@ if load is False:
     varDifferences=scipy.io.loadmat('varDiffweighted.mat')['out']
     contt=scipy.io.loadmat('contDiffweighted.mat')['out']
     
+    differences2=scipy.io.loadmat('largebetaweighteddifferences.mat')['out']
+    
 
-minD=differences.min()*100
-maxD=12
+minD=np.min(differences.min(),differences2.min())*100
+maxD=np.max(0.12,differences2.max())*100
 print minD,maxD
 ###betah
+
 x=np.linspace(0,samplesIteration[0]*numberIterations,numberIterations+1)
-BETA,ITERATIONS=np.meshgrid(varianceb,x)
+BETA,ITERATIONS=np.meshgrid(2.0*varianceb,x)
 
 Z=np.zeros(BETA.shape)
 Z2=np.zeros(BETA.shape)
 
 for i in xrange(BETA.shape[0]):
-    for j in range(0,BETA.shape[1]):
+    for j in range(0,5):
 	Z[i,j]=100.0*differences[j,0,0,i]
 	Z2[i,j]=100.0*differences[j,3,0,i]
+    for j in xrange(5,BETA.shape[1]):
+	Z[i,j]=100.0*differences2[j-5,0,0,i]
+	Z2[i,j]=100.0*differences2[j-5,1,0,i]
 
 #norm=cm.colors.Normalize(vmax=abs(Z).max(), vmin=-abs(Z).max())
 #cmap=cm.PRGn 
@@ -142,7 +148,7 @@ plt.colorbar()
 plt.title("Percent Increase between SBO and KG. N is 1 and A is 0.5")
 plt.ylabel("Number of samples")
 plt.xlabel("beta")
-plt.savefig(os.path.join("plots","contourPlotbetahN1A2"+".pdf"))
+plt.savefig(os.path.join("plots","contourPlotbetahN1A2ver2"+".pdf"))
 plt.close("all")
 
 fig = plt.figure()
@@ -155,100 +161,7 @@ plt.colorbar()
 plt.title("Percent Increase between SBO and KG. N is 8 and A is 0.06")
 plt.ylabel("Number of samples")
 plt.xlabel("beta")
-plt.savefig(os.path.join("plots","contourPlotbetahN8A2"+".pdf"))
+plt.savefig(os.path.join("plots","contourPlotbetahN8A2ver2"+".pdf"))
 plt.close("all")
 
 
-####N
-
-N,ITERATIONS=np.meshgrid(samplesIteration,x)
-
-Z=np.zeros(N.shape)
-Z2=np.zeros(N.shape)
-Z3=np.zeros(N.shape)
-
-for i in xrange(N.shape[0]):
-    for j in range(0,N.shape[1]):
-        Z[i,j]=100.0*differences[0,j,0,i]
-        Z2[i,j]=100.0*differences[4,j,0,i]
-        Z3[i,j]=100.0*differences[4,j,1,i]
-#norm=cm.colors.Normalize(vmax=abs(Z).max(), vmin=-abs(Z).max())
-#cmap=cm.PRGn
-fig = plt.figure()
-cset1 = plt.contourf(N, ITERATIONS, Z,
-                 cmap=cm.jet,vmin=minD,vmax=maxD,
-                 )
-plt.colorbar()
-       #m = cm.ScalarMappable(cmap=cm.jet)
-    #m.set_array(Z)
-plt.title("Percent Increase between SBO and KG. Beta is 0.06")
-plt.ylabel("Number of samples")
-plt.xlabel("Samples per Iteration")
-plt.savefig(os.path.join("plots","contourPlotNbeta0A0"+".pdf"))
-plt.close("all")
-
-fig = plt.figure()
-cset1 = plt.contourf(N, ITERATIONS, Z2,
-                 cmap=cm.jet,vmin=minD,vmax=maxD,
-                 )
-plt.colorbar()
-       #m = cm.ScalarMappable(cmap=cm.jet)
-    #m.set_array(Z)
-plt.title("Percent Increase between SBO and KG. Beta is 1.")
-plt.ylabel("Number of samples")
-plt.xlabel("Samples per Iteration")
-plt.savefig(os.path.join("plots","contourPlotNbeta4A0"+".pdf"))
-
-
-fig = plt.figure()
-cset1 = plt.contourf(N, ITERATIONS, Z3,
-                 cmap=cm.jet,vmin=minD,vmax=maxD,
-                 )
-plt.colorbar()
-       #m = cm.ScalarMappable(cmap=cm.jet)
-    #m.set_array(Z)
-plt.title("Percent Increase between SBO and KG. Beta is 1.")
-plt.ylabel("Number of samples")
-plt.xlabel("Samples per Iteration")
-plt.savefig(os.path.join("plots","contourPlotNbeta4A3"+".pdf"))
-
-######Change A
-
-Aparam=[1.0/(A[j]*1.0) for j in range(4)]
-Agrid,ITERATIONS=np.meshgrid(Aparam,x)
-
-Z=np.zeros(Agrid.shape)
-Z2=np.zeros(Agrid.shape)
-
-for i in xrange(Agrid.shape[0]):
-    for j in range(0,Agrid.shape[1]):
-        Z[i,j]=100.0*differences[0,0,j,i]
-        Z2[i,j]=100.0*differences[4,0,j,i]
-
-
-#norm=cm.colors.Normalize(vmax=abs(Z).max(), vmin=-abs(Z).max())
-#cmap=cm.PRGn
-fig = plt.figure()
-cset1 = plt.contourf(Agrid, ITERATIONS, Z,
-                 cmap=cm.jet,vmin=minD,vmax=maxD,
-                 )
-plt.colorbar()
-       #m = cm.ScalarMappable(cmap=cm.jet)
-    #m.set_array(Z)
-plt.title("Percent Increase between SBO and KG. Beta is 0.06 and N is 1")
-plt.ylabel("Number of samples")
-plt.xlabel("A")
-plt.savefig(os.path.join("plots","contourPlotAbeta0N0"+".pdf"))
-plt.close("all")
-
-fig = plt.figure()
-cset1 = plt.contourf(Agrid, ITERATIONS, Z2,
-                 cmap=cm.jet,vmin=minD,vmax=maxD,
-                 )
-plt.colorbar()
-       #m = cm.ScalarMappable(cmap=cm.jet)
-    #m.set_array(Z)
-plt.title("Percent Increase between SBO and KG. Beta is 1 and N is 1")
-plt.ylabel("Number of samples")
-plt.xlabel("A")
-plt.savefig(os.path.join("plots","contourPlotAbeta4N0"+".pdf"))
