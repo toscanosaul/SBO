19cbfd7f45e0c72650a210737f3b460524d7dc7f
Saul Toscano <toscano.saul@gmail.com>
2016-05-14 04:02:24 -0400

    ,

--- BGOis/Source/__init__.py
+++ BGOis/Source/__init__.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
-from . import inter,KG,SBO,SK,EI,stat,VOI,mattern52
+from . import inter,KG,SBO,SK,EI,stat,VOI,mattern52,files
 
-__all__=["inter","KG","SBO","SK","EI","stat","VOI","mattern52"]
+__all__=["inter","KG","SBO","SK","EI","stat","VOI","mattern52","files"]
