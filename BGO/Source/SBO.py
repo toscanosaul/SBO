b4d407900e2cd8fe483a4209155a63864c50c01b
Saul Toscano <toscano.saul@gmail.com>
2016-05-18 01:13:16 -0400

    ,

--- BGO/Source/SBO.py
+++ BGO/Source/SBO.py
@@ -337,7 +337,8 @@ class SBO:
 			optVOI=sols[t][j].fOpt
 			optPoint=sols[t][j]
 			optIS=t
-
+	    print "solution"
+	    print [[o.fOp] for o in sols[t] for t in range(self.nIS)]
 	    optPoint.xOpt=np.concatenate((optPoint.xOpt,np.array([[optIS]])),1)
 	    fl.writeNewPointSBO(self,optPoint)
 	else:
